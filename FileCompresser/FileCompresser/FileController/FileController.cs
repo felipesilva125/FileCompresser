ee4d4d2ee22e7d53e8cec20059db570d00867607
Felipe Silva <felipe.silva@ska.com.br>
2020-09-12 18:16:24 -0300

    First version, implemented reading and writing of a file

---
+++ FileCompresser/FileCompresser/FileController/FileController.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace FileCompresser
+{
+    public static class FileController
+    {
+        private const string FILE_PATH = @"C:\Temp";
+        private const string READING_EXTENSION = "txt";
+        private const string COMPRESSING_EXTENSION = "cod";
+        private const string DECOMPRESSING_EXTENSION = "dec";
+
+        public static string ReadFileContent(string fileName)
+        {            
+            var path = getFullPath(fileName, READING_EXTENSION);
+
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File {fileName} not found!");
+
+            return File.ReadAllText(path);
+        }
+
+        public static void WriteFileContent(string fileName, string fileContent)
+        {
+            var path = getFullPath(fileName, COMPRESSING_EXTENSION);
+
+            if (File.Exists(path))
+                throw new FileNotFoundException($"File {fileName} already exists!");
+
+            File.WriteAllText(path, fileContent);
+        }
+
+        private static string getFullPath(string fileName, string extension)
+        {
+            fileName = Path.ChangeExtension(fileName, extension);
+            var path = Path.Combine(FILE_PATH, fileName);
+
+            return path;
+        }
+    }        
+}
---
+++ FileCompresser/FileCompresser/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FileCompresser
+{
+    public class Program
+    {
+        public static void Main()
+        {            
+            try
+            {
+                compressFile("alice29");
+                //decompressFile("alice29");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");                
+            }
+        }        
+
+        private static void compressFile(string fileName)
+        {
+            var fileContent = FileController.ReadFileContent(fileName);
+
+            //passa pelo encoder
+
+            FileController.WriteFileContent(fileName, fileContent);
+        }
+
+        private static void decompressFile(string fileName)
+        {
+            //lê arquivo
+            var fileContent = FileController.ReadFileContent(fileName);
+
+            //passa pelo decoder
+
+            //grava arquivo
+            FileController.WriteFileContent(fileName, fileContent);
+        }
+    }
+}
